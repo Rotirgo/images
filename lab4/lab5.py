f626f3b68778efd928c35a088d25b44081a48272
Nikita Kozlov <kozlov22.00@mail.ru>
2022-11-04 13:48:35 +0400

    доделать графики.

--- lab4/lab5.py
+++ lab4/lab5.py
@@ -1,5 +1,4 @@
 import copy
-
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.io import imsave, imshow, show, imread
@@ -96,12 +95,11 @@ def maxminMethod(vectors):
             distanceTable.append(Distance(result, arrM[i]))
         # распределить по существующим кластерам
         l = np.argmin(np.transpose(distanceTable), axis=1)
-        tmp = copy.copy(clusters)
+        tmp = copy.deepcopy(clusters)
         for k in range(0, len(result)):
             tmp[l[k]].append(result[k])
 
         # отобразить результат
-        # arrFig.append(viewClusters(tmp, arrM))
         fig0 = plt.figure(figsize=(10, 10))
         viewClusters(tmp, arrM, fig0, 111, legend=legends)
         # show()
@@ -120,18 +118,68 @@ def maxminMethod(vectors):
                 dtypical[-1] += np.sum(Distance(arrM, arrM[j]))
             dtypical[-1] /= 2*len(arrM)*(len(arrM) - 1)
     dmin.pop(0)
+    for k in range(0, len(result)):
+        clusters[l[k]].append(result[k])
     return clusters, dmin, dtypical, arrM
 
 
-def K_introGroupAvg(vectors, startVal, K):
+def K_introGroupAvg(vectors, initVectors):
+    K = len(np.transpose(initVectors))
+    clusters = []
+    legends = ["M(x)"]
+    for i in range(0, K):
+        clusters.append([])
+        legends.append(f"class {i}")
 
-    return 0
+    result = np.transpose(vectors)
+    new_arrM = copy.deepcopy(np.transpose(initVectors))
+    prev_arrM = np.mean(result, axis=0)*np.ones_like(new_arrM)
+    prev_k = np.zeros((len(result),)).astype(int)
+    imposters = [-1]
+
+    tmp = [result]
+    while not (imposters[-1] == 0):  # not ((imposters[-1] == 0) | (new_arrM == prev_arrM).all())
+        distances = []
+        for i in range(0, K):
+            distances.append(Distance(result, new_arrM[i]))
+        # принадлежность классам
+        new_k = np.argmin(distances, axis=0)
+
+        # отобразить предыдущее разбиение
+        fig0 = plt.figure(figsize=(16, 7))
+        viewClusters(tmp, prev_arrM, fig0, 121, legends)
+
+        # разбиение по классам
+        copyClusters = copy.deepcopy(clusters)
+        for i in range(0, len(result)):
+            copyClusters[new_k[i]].append(result[i])
+
+        # отобразить текущее разбиение
+        tmp = copy.copy(copyClusters)
+        viewClusters(copyClusters, new_arrM, fig0, 122, legends)
+
+        # количество изменивших класс
+        values, counts = np.unique(new_k == prev_k, return_counts=True)
+        if False in list(values):
+            imposters.append(counts[list(values).index(False)])
+        else:
+            imposters.append(0)
+        prev_k = copy.copy(new_k)
+
+        # обновление мат.ожиданий
+        prev_arrM = copy.copy(new_arrM)
+        for i in range(0, K):
+            new_arrM[i] = np.mean(copyClusters[i], axis=0)
+    imposters.pop(0)
+    for j in range(0, len(result)):
+        clusters[new_k[j]].append(result[j])
+    return clusters, new_arrM, imposters
 
 
-def viewClusters(data, arrM, fig, loc, legend):
+def viewClusters(datas, arrM, fig, loc, legend):
     viewData = []
-    for k in range(0, len(data)):
-        viewData.append(np.transpose(data[k]))
+    for k in range(0, len(datas)):
+        viewData.append(np.transpose(datas[k]))
     tmp = np.transpose(arrM)
 
     fig.add_subplot(loc)
@@ -143,6 +191,21 @@ def viewClusters(data, arrM, fig, loc, legend):
         plt.plot(viewData[i][0], viewData[i][1], c[i % len(c)])
     plt.legend(legend)
     return fig
+
+
+def viewResultAndOriginal(classes, Ms, legends):
+    fig = plt.figure(figsize=(16, 7))
+    fig.add_subplot(121)
+    plt.xlim(-1.6, 1.6)
+    plt.ylim(-0.6, 2.6)
+    plt.plot(x1[0], x1[1], 'r.')
+    plt.plot(x2[0], x2[1], 'gx')
+    plt.plot(x3[0], x3[1], 'b<')
+    plt.plot(x4[0], x4[1], 'm*')
+    plt.plot(x5[0], x5[1], 'c+')
+    plt.legend(legs)
+    viewClusters(classes, Ms, fig, 122, legends)
+    show()
 
 
 def print_hi(name):
@@ -191,6 +254,15 @@ if __name__ == '__main__':
     plt.legend(["d min", "d typical"])
     show()
 
-    classes3 = K_introGroupAvg(data, 1, 5)
+    classes3, M3, imposter3 = K_introGroupAvg(data, data[:, [151, 156, 160]])
+    show()
+
+    classes5, M5, imposters5 = K_introGroupAvg(data, data[:, [151, 156, 160, 161, 166]])
+    viewResultAndOriginal(classes5, M5, legs)
+
+    fakeClasses5, fakeM5, fakeImposters5 = K_introGroupAvg(data, data[:, 0:5])  # подумать над начальными
+                                                                                # для неправильной
+    viewResultAndOriginal(fakeClasses5, fakeM5, legs)
+    show()
 
 
